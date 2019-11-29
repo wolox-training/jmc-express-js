9e61dc9cdb3e883e416896f38c86069f21869780
Jhon Munoz <jhonathan.munoz@wolox.cl>
2019-11-29 15:51:47 -0300

    refactor on conditional to keep it DRY

--- app/controllers/helpers/request.js
+++ app/controllers/helpers/request.js
@@ -4,18 +4,8 @@ const { logger } = require('../../logger/index');
 const request = options =>
   new Promise((resolve, reject) => {
     requestPackage[options.method](options, (error, response) => {
-      if (error) {
-        // eslint-disable-next-line no-console
-        console.log(error.message);
+      if (error || !response.statusCode || response.statusCode !== 200) {
         logger.error(Object.assign(error, options));
-        return reject(error);
-      }
-      if (response.statusCode !== 200) {
-        return reject(error);
-      }
-      if (!response.statusCode) {
-        // eslint-disable-next-line no-console
-        console.log(error.message);
         return reject(error);
       }
       return resolve(response);
