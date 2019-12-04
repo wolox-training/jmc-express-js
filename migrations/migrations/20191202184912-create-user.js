38dd9b47ddf8f572e950eae70073ff482f60a275
Jhon Munoz <jhonathan.munoz@wolox.cl>
2019-12-04 12:27:57 -0300

    modifies migration to add index and snake cases table names

--- migrations/migrations/20191202184912-create-user.js
+++ migrations/migrations/20191202184912-create-user.js
@@ -1,33 +1,39 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }),
-  down: queryInterface => queryInterface.dropTable('Users')
+    queryInterface
+      .createTable('users', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        first_name: {
+          type: Sequelize.STRING
+        },
+        last_name: {
+          type: Sequelize.STRING
+        },
+        email: {
+          type: Sequelize.STRING
+        },
+        password: {
+          type: Sequelize.STRING
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })
+      .then(() => {
+        queryInterface.addIndex('users', ['email'], {
+          unique: true
+        });
+      }),
+  down: queryInterface => queryInterface.dropTable('users')
 };
